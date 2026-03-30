5ffdacf19f9f9e9f9ae79b0380cbfcdebf40c8c4
Claire Rosenthal <113315102+claire-therose@users.noreply.github.com>
2026-03-30 12:47:29 -0700

    changed titlebar to lowercase to fit with theme

--- src/app/components/titlebar-logo.tsx
+++ src/app/components/titlebar-logo.tsx
@@ -6,7 +6,7 @@ export default function TitlebarLogo () {
       <section className='w-8 h-8 ml-1 p-1'>
         <img src={cabnetLogoMain} alt='App logo' />
       </section>
-      <section className='ml-1 font-title font-semibold text-[16px]'>Cabnet Player</section>
+      <section className='ml-1 font-title font-semibold text-[16px]'>cabnet player</section>
     </>
   );
 }
